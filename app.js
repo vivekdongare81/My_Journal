973ed79871b7b071d8b87dd377473bc3ac3588d6
Vivek <dongarevivek81@gmail.com>
2021-10-17 23:06:12 +0530

    Second comm

--- app.js
+++ app.js
@@ -67,6 +67,6 @@ app.get("/posts/:postName", function (req, res) {
   else { console.log("Not Found"); }
 });
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server started ...");
 });
